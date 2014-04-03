48e45470a713a9954412559a470751f32b8ae3a3
Joas de Haan <jdehaan@servoy.com>
2014-04-03 15:58:15 +0200

    fixed error

--- testGIT/forms/form1.js
+++ testGIT/forms/form1.js
@@ -17,10 +17,3 @@ function coolfunction(){
 function onActionDie(event) {
 	application.exit();
 }
-
-/**
- * @properties={typeid:24,uuid:"75BA2060-F9AE-4BAC-8F39-4FE1438D4DBD"}
- */
-function coolfunction(){
-	application.output('HI')
-}
