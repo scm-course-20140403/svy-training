1c3a17d89458276ae08033242a9ac928406816f8
Sean Devlin <sdevlin@servoy.com>
2014-04-03 06:29:56 -0400

    Changed method1

--- testGIT/globals.js
+++ testGIT/globals.js
@@ -3,5 +3,8 @@
  * @properties={typeid:24,uuid:"EE839DC7-C35A-49CD-965B-E7475175F3A9"}
  */
 function method1() {
-	application.output('FOO');
+	//	changed in feature branch
+	application.output('Foobar');
+	
+	
 }
